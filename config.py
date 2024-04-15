9c310453ac75e4834a16802230e1a737b09ee18b
mahcopration <129929195+mahcopration@users.noreply.github.com>
2024-04-16 01:57:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "10917377")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "22bfe49f4482c5cb5424729249a5f097") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7064479547:AAGUOtEwRxad3xY7E78B5A48TWenQPsGIuU") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '-1001938180288') # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'Call_me_futurepilot') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://videoencodemass:videoencodemass@cluster0.yns1vfu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # ⚠️ Required
     DB_NAME  = os.environ.get("DB_NAME","VideoEncoderFastBot") 
 
     # Other Configs 
